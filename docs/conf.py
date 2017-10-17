3767bd184d3bcf7c5f556125351feb6fa1740c77
Daithi O Crualaoich <daithiocrualaoich@users.noreply.github.com>
2017-10-17 01:46:56 +0100

    Release 1.0.1 (#45)

--- docs/conf.py
+++ docs/conf.py
@@ -6,8 +6,8 @@ project = u'RktMachine'
 copyright = u'2017, Woof Woof, Inc.'
 author = u'Woof Woof, Inc.'
 
-version = '1.0.0'
-release = '1.0.0'
+version = '1.0.1'
+release = '1.0.1'
 
 templates_path = ['_templates']
 exclude_patterns = ['_build']
